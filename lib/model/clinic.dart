d47cdf3d8ea4d572a72922a325e2c2ae48a5bbb9
Yusuf Hijriah <yusufhijriah19@gmail.com>
2021-12-09 07:18:55 +0800

    melengkapi data pada clinic.dart dan clinic.json

--- lib/model/clinic.dart
+++ lib/model/clinic.dart
@@ -16,12 +16,13 @@ class Clinics {
   List<Clinic> clinics;
 
   factory Clinics.fromJson(Map<String, dynamic> json) => Clinics(
-    clinics: List<Clinic>.from(json["clinics"].map((x) => Clinic.fromJson(x))),
-  );
+        clinics:
+            List<Clinic>.from(json["clinics"].map((x) => Clinic.fromJson(x))),
+      );
 
   Map<String, dynamic> toJson() => {
-    "clinics": List<dynamic>.from(clinics.map((x) => x.toJson())),
-  };
+        "clinics": List<dynamic>.from(clinics.map((x) => x.toJson())),
+      };
 }
 
 class Clinic {
@@ -33,6 +34,9 @@ class Clinic {
     required this.city,
     required this.telp,
     required this.rating,
+    required this.workingHours,
+    required this.service,
+    required this.price,
   });
 
   String id;
@@ -42,24 +46,33 @@ class Clinic {
   String city;
   String telp;
   double rating;
+  String workingHours;
+  String service;
+  String price;
 
   factory Clinic.fromJson(Map<String, dynamic> json) => Clinic(
-    id: json["id"],
-    name: json["name"],
-    address: json["address"],
-    pictureId: json["pictureId"],
-    city: json["city"],
-    telp: json["telp"],
-    rating: json["rating"].toDouble(),
-  );
+        id: json["id"],
+        name: json["name"],
+        address: json["address"],
+        pictureId: json["pictureId"],
+        city: json["city"],
+        telp: json["telp"],
+        rating: json["rating"].toDouble(),
+        workingHours: json["workingHours"],
+        service: json["service"],
+        price: json["price"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "name": name,
-    "address": address,
-    "pictureId": pictureId,
-    "city": city,
-    "telp": telp,
-    "rating": rating,
-  };
+        "id": id,
+        "name": name,
+        "address": address,
+        "pictureId": pictureId,
+        "city": city,
+        "telp": telp,
+        "rating": rating,
+        "workingHours": workingHours,
+        "service": service,
+        "price": price,
+      };
 }
