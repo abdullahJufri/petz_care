c02f5ab9859b46adf3ddba174366f618bfaae8f8
Yusuf Hijriah <yusufhijriah19@gmail.com>
2021-12-18 22:04:59 +0800

    Membuat appointment

--- lib/ui/detail_page.dart
+++ lib/ui/detail_page.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:petz_care/firestore/database.dart';
 import 'package:petz_care/firestore/query_controller.dart';
 import 'package:petz_care/theme.dart';
+import 'package:petz_care/widget/flushbar.dart';
+import 'package:petz_care/widget/text_fied.dart';
 
 class DetailPage extends StatefulWidget {
   // final String clinic;
@@ -21,6 +25,9 @@ class DetailPage extends StatefulWidget {
 
 class _DetailPageState extends State<DetailPage> {
   TextEditingController nameController = new TextEditingController();
+  final _date = TextEditingController();
+  final _time = TextEditingController();
+  final _service = TextEditingController();
 
   @override
   void initState() {
@@ -184,13 +191,148 @@ class _DetailPageState extends State<DetailPage> {
                               width:
                                   MediaQuery.of(context).size.width - (2 * 5),
                               child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.blueGrey,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
                                   ),
-                                  onPressed: () {},
-                                  child: Text('Book Now')),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.calendar_today,
+                                      size: 17,
+                                    ),
+                                    SizedBox(width: 5),
+                                    Text('Make an Appointment'),
+                                  ],
+                                ),
+                                onPressed: () {
+                                  showModalBottomSheet(
+                                    context: context,
+                                    isScrollControlled: true,
+                                    elevation: 1,
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.vertical(
+                                        top: Radius.circular(20),
+                                      ),
+                                    ),
+                                    builder: (context) => Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Container(
+                                          margin: const EdgeInsets.fromLTRB(
+                                              0, 8, 0, 30),
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.1,
+                                          height: 4,
+                                          decoration: BoxDecoration(
+                                            color: Colors.grey.shade700,
+                                            borderRadius:
+                                                BorderRadius.circular(6),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 20,
+                                          ),
+                                          child: FieldDateTime(
+                                            ctrlText: _date,
+                                            hint: "Select Date",
+                                            disableBackDate: true,
+                                            borderOutline: false,
+                                            onSaved: (val) {},
+                                          ),
+                                        ),
+                                        const SizedBox(height: 10),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 20,
+                                          ),
+                                          child: FieldDateTime(
+                                            ctrlText: _time,
+                                            hint: "Select Time",
+                                            timeInput: true,
+                                            borderOutline: false,
+                                            onSaved: (val) {},
+                                          ),
+                                        ),
+                                        const SizedBox(height: 10),
+                                        Padding(
+                                          padding: EdgeInsets.only(
+                                            left: 20,
+                                            right: 20,
+                                            bottom: MediaQuery.of(context)
+                                                .viewInsets
+                                                .bottom,
+                                          ),
+                                          child: FieldInput(
+                                            controller: _service,
+                                            hint: "Enter the desired service",
+                                            maxLength: 50,
+                                          ),
+                                        ),
+                                        GestureDetector(
+                                          onTap: () {
+                                            if (_date.text.isEmpty) {
+                                              return Alert.error(
+                                                context: context,
+                                                message: "Please choose a date",
+                                              );
+                                            }
+
+                                            if (_time.text.isEmpty) {
+                                              return Alert.error(
+                                                context: context,
+                                                message: "Please choose a time",
+                                              );
+                                            }
+
+                                            if (_service.text.isEmpty) {
+                                              return Alert.error(
+                                                context: context,
+                                                message: "Please input service",
+                                              );
+                                            }
+                                            // DateTime date = DateTime.parse(_date.text);
+                                            // String pickedTimeFormat = DateFormat("EEEE").format(date);
+                                            // log(pickedTimeFormat.toString());
+
+                                            log(_date.text +
+                                                " , " +
+                                                _time.text);
+                                          },
+                                          child: Container(
+                                            width: double.infinity,
+                                            margin: const EdgeInsets.symmetric(
+                                              horizontal: 40,
+                                              vertical: 25,
+                                            ),
+                                            padding: const EdgeInsets.all(15),
+                                            decoration: BoxDecoration(
+                                              color: Colors.blueGrey,
+                                              borderRadius:
+                                                  BorderRadius.circular(50),
+                                            ),
+                                            child: const Center(
+                                              child: Text(
+                                                "Make Appointment",
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w500,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                              ),
                             )
                           ],
                         ),
